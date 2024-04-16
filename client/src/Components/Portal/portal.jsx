8035e29e6f289188d39b09a85cafec9e7386e20c
Ahmad-Hassan-0 <mirzaahmadhassan786@gmail.com>
2024-04-16 23:21:04 +0500

    init

--- client/src/Components/Portal/portal.jsx
+++ client/src/Components/Portal/portal.jsx
@@ -1,12 +1,41 @@
-//import {useState} from 'react'
+import {useState} from 'react'
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import './portal.css'
 
 const portal = () => {
 
 //  const [thing, thingsetter] = useState("");
 
+const [rowData, setRowData] = useState([
+  { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  { make: "Ford", model: "F-Series", price: 33850, electric: false },
+  { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+]);
+
+// Column Definitions: Defines the columns to be displayed.
+const [colDefs, setColDefs] = useState([
+  { field: "make" },
+  { field: "model" },
+  { field: "price" },
+  { field: "electric" }
+]);
+
+const rowClassRules = {
+    // apply red to Ford cars
+    'rag-red': params => params.data.make === 'Ford',
+};
+
   return (
-    <div>
-      this is the portals
+    <div className="ag-theme-quartz" >
+
+   <AgGridReact
+       rowData={rowData}
+       columnDefs={colDefs}
+       rowClassRules={rowClassRules}
+   />
+
     </div>
   )
 }
